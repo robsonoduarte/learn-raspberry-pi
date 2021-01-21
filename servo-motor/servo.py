abf01886c981dfcc61303d8cf276e1830d4e66c6
Robson Duarte <robson.o.d@gmail.com>
2021-01-21 13:36:28 -0300

    :pray:

--- servo-motor/servo.py
+++ servo-motor/servo.py
@@ -1,6 +1,13 @@
 from gpiozero import Servo
+from time import sleep
 
 #https://gpiozero.readthedocs.io/en/stable/recipes.html#servo
 
-servo = Servo(17)
+servo = Servo(14)
 
+
+servo.min()
+sleep(2)
+servo.min()
+sleep(2)
+servo.max()
