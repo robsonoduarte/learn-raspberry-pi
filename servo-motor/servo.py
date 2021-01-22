e9e648ae0fb738afea50f6d5ed71588b846b0c11
Robson Duarte <robson.o.d@gmail.com>
2021-01-22 12:10:38 -0300

    :pray:

--- servo-motor/servo.py
+++ servo-motor/servo.py
@@ -5,9 +5,9 @@ from time import sleep
 
 servo = Servo(17)
 
-
-servo.min()
-sleep(2)
-servo.mid()
-sleep(2)
-servo.max()
+while True:
+    servo.min()
+    sleep(2)
+    servo.mid()
+    sleep(2)
+    servo.max()
